739f75b98b9178e9c2acfa44be1de11a89925066
TATaewoo <limstudio@nm-fc.com>
2023-03-03 10:46:42 +0900

    first commit

---
+++ Source/UnrealPerfCompare/UnrealPerfCompareGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealPerfCompareGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPERFCOMPARE_API AUnrealPerfCompareGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
